269e64628ff8a436190552046f2d4fff269c26a5
wangsiji <wangsiji@buaa.edu.cn>
2020-03-31 09:32:11 +0800

    LT

--- JianZhiOffer/面试题65_不用加减乘除做加法.py
+++ JianZhiOffer/面试题65_不用加减乘除做加法.py
@@ -66,19 +66,18 @@ class Solution(object):
         #     a = plus
         # return a
 
-
-
         # (n & 0xffffffff)进行这种变换的原因是,如果存在负数则需要转换成补码的形式,正数补码是他本身
-        a &= 0xffffffff#
+        a &= 0xffffffff  #
         b &= 0xffffffff
         while b != 0:
-            carry = ((a & b) << 1) & 0xffffffff#如果是负数,转换成补码形式
+            carry = ((a & b) << 1) & 0xffffffff  # 如果是负数,转换成补码形式
             a ^= b
             b = carry
-        if a < 0x80000000:#如果是正数的话直接返回
+        if a < 0x80000000:  # 如果是正数的话直接返回
             return a
         else:
-            return  ~(a^0xffffffff)#是负数的话,转化成其原码
+            return ~(a ^ 0xffffffff)  # 是负数的话,转化成其原码
+
 
 if __name__ == "__main__":
     a = 13
