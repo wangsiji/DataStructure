0eb5d8952c45521de9bc41b7f4f37d134dd2b13f
wangsiji <wangsiji@buaa.edu.cn>
2020-03-31 09:55:07 +0800

    LT

--- LeetCode/053_最大子序和.py
+++ LeetCode/053_最大子序和.py
@@ -34,8 +34,6 @@ class Solution(object):
         """
 
         '''空间复杂度O(n)'''
-        if len(nums) < 2:
-            return nums[0]
         dp = [0 for _ in range(len(nums))]
         dp[0] = nums[0]
         for i in range(1, len(nums)):
@@ -52,5 +50,5 @@ class Solution(object):
 
 
 if __name__ == "__main__":
-    nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    nums = [-2, 1]
     print(Solution().maxSubArray(nums))
