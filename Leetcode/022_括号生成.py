9af9d0fd231e026f204df262b844e8e5025f20d6
wangsiji <wangsiji@buaa.edu.cn>
2020-04-16 11:50:04 +0800

    回溯

--- Leetcode/022_括号生成.py
+++ Leetcode/022_括号生成.py
@@ -16,7 +16,8 @@
   "()()()"
 ]
 """
-# TODO
+
+'''https://leetcode-cn.com/problems/generate-parentheses/solution/gua-hao-sheng-cheng-by-leetcode-solution/'''
 
 class Solution(object):
     def generateParenthesis(self, n):
@@ -24,26 +25,73 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        pass
         '''
-        方法一 暴力
+        方法一 暴力解法
+        我们可以生成所有 2**2n个 '(' 和 ')' 字符构成的序列，然后我们检查每一个是否有效即可。
+        
+        时间复杂度：O（n * 2 ** 2n）
+        空间复杂度：O(n)
         '''
-        def generateParenthesis(n):
-            combinations = list()
-            generateAll([0 for _ in range(2 * n)], 0, combinations)
-        def generateAll(current, pos, res):
-            if pos == len(current):
-                if valid(current):res.append(current)
-
-        def valid():
-            pass
-        if n == 0: return [""]
-        res = []
-        for c in range(n):
-            pass
-        # '''
-        # 方法二 回溯法
-        # '''
-        # def backtrack(res, cur, left,right, max):
+        # def generate(A):
+        #     """
+        #     为了生成所有序列，我们可以使用递归。长度为 n 的序列就是在长度为 n-1 的序列前加一个 '(' 或 ')'。
+        #     """
+        #     if len(A) == 2 * n:
+        #         if valid(A):
+        #             res.append("".join(A))
+        #     else:
+        #         A.append("(")
+        #         generate(A)
+        #         A.pop()
+        #         A.append(")")
+        #         generate(A)
+        #         A.pop()
+        # def valid(A):
+        #     bal = 0
+        #     for c in A:
+        #         if c == "(":
+        #             bal += 1
+        #         else:
+        #             bal -= 1
+        #         if bal < 0: return False
+        #     return bal == 0
         #
+        # res = []
+        # generate([])
+        # return res
+
+        '''
+        方法二 回溯法
+        我们可以只在序列仍然保持有效时才添加 '(' or ')'，而不是像 方法一 那样每次添加。
+        
+        可以通过跟踪到目前为止放置的左括号和右括号的数目来做到这一点
+        
+        如果左括号数量不大于n，我们可以放一个左括号。如果右括号数量小于左括号的数量，我们可以放一个右括号
+        '''
+
+
+        '''
+        ['((()))', '(()())', '(())()', '()(())', '()()()']
+        '''
+        res = []
+
+        def backtrack(S, left, right):
+            if len(S) == 2 * n:
+                res.append("".join(S))
+                return
+            if left < n :
+                S.append("(")
+                backtrack(S, left+1, right)
+                S.pop()
+            if right < left:
+                S.append(")")
+                backtrack(S, left, right+1)
+                S.pop()
+
+        backtrack([], 0, 0)
+        return res
+
+
 if __name__ == "__main__":
     print(Solution().generateParenthesis(3))
